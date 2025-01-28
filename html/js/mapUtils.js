92beda5895285e5badc8e5ec090556790e85da8a
martinantonmueller <martin.anton.mueller@oeaw.ac.at>
2025-01-28 09:46:43 +0100

    popup-probleme

--- html/js/mapUtils.js
+++ html/js/mapUtils.js
@@ -140,7 +140,7 @@ function createPopupContent(feature) {
     }
 
     return `
-<div class="modal fade show" tabindex="-1" role="dialog" style="display: block;">
+    <div class="modal fade" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
