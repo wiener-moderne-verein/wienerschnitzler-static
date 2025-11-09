f553a05110810e24ea36c9097d8b022613c43dfe
martinantonmueller <martin.anton.mueller@oeaw.ac.at>
2025-11-09 23:09:13 +0100

    nochmals wegen saxon

--- shellscripts/dl_saxon.sh
+++ shellscripts/dl_saxon.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "downloading saxon"
 curl -LO https://sourceforge.net/projects/saxon/files/Saxon-HE/9.9/SaxonHE9-9-1-7J.zip/download
-unzip download -d saxon
+mkdir -p saxon
+unzip -o download -d saxon
 rm -rf download
+echo "Saxon downloaded and extracted successfully"
+ls -la saxon/
