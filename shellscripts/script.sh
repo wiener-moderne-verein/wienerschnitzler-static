37697e925a04a510331dc249dbf2c2c2c82c741c
martinantonmueller <martin.anton.mueller@oeaw.ac.at>
2025-11-09 23:06:52 +0100

    fix script

--- shellscripts/script.sh
+++ shellscripts/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# hacky way to make shure the script 
-# gets allways run from parent-dir 
+# hacky way to make shure the script
+# gets allways run from parent-dir
 # so relative paths get resolved the righ way
-script_location_dir="${BASH_SOURCE[0]}"
-cd $script_location_dir && cd ..
+script_location_dir=$(dirname "${BASH_SOURCE[0]}")
+cd "$script_location_dir/.."
 # run scripts
 ./shellscripts/dl_saxon.sh
